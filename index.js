017502ebec31181e15ddb8fd963a6fc8ba1dc005
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-05-14 17:15:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
-/* Your Code Here */
+function createEmployeeRecord(array){
+return Object = {
+firstName:array[0],
+familyName:array[1],
+title:array[2],
+payPerHour:array[3],
+timeInEvents:[],
+timeOutEvents:[]
+    }
+}
+function createEmployeeRecords(arrayOfArrays) {
+    const result = arrayOfArrays.map(record => {
+        return createEmployeeRecord(record)
+    } )
+    return result 
+}
+function createTimeInEvent(date){
+    const newTimeObj = createTimeEvent(date, "TimeIn")
+    this.timeInEvents.push(newTimeObj)
+return this
+}
 
+function createTimeOutEvent(date){
+    this.timeOutEvents.push(createTimeEvent(date, "TimeOut"))
+    return this
+    }
+
+function  hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(record => record.date===date)
+    const timeOut = this.timeOutEvents.find(record => record.date===date)
+    return (timeOut.hour -timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+    const numHours = hoursWorkedOnDate.call(this, date)
+    return numHours*this.payPerHour
+}
+
+function findEmployeeByFirstName(records, firstName) {
+    const found = records.find(record => record.firstName === firstName);
+    return found 
+}
+
+function calculatePayroll(records){
+    return records.reduce((acc,record) =>{
+        return allWagesFor.call(record)+acc
+    },0)
+    }
+``
+
+
+
+//Helpers
+function createTimeEvent(date,type){
+        return{
+        type,
+        hour: parseInt(date.split(" ")[1]),
+        date: date.split(" ")[0]
+        }  
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -7,13 +65,12 @@
 
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
- */
+//  */
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
-
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
